c828fa72c99aa8d9f1ddd203022e6158453dab81
coltonleethornburgh <coltonleethornburgh@gmail.com>
2025-11-11 09:14:31 -0800

    adding script to move file

---
+++ mv-config.ps1
@@ -0,0 +1,26 @@
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -ErrorAction Stop
+# --- CONFIG ---
+$steamLibs = @(
+    "$env:ProgramFiles (x86)\Steam",
+    "$env:ProgramFiles\Steam"
+)
+
+$cs2Path = $null
+$cs2Path = $null
+foreach ($steamBase in $steamLibs) {
+    $testPath = Join-Path $steamBase "steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+    if (Test-Path $testPath) {
+        $cs2Path = $testPath
+        break
+    }
+}
+
+
+if (-not $cs2Path) {
+    Write-Host "Could not find CS2 installation directory."
+    exit 1
+}
+
+# Copy the new file
+Copy-Item  ".\autoexec.cfg" $cs2Path -Force
+Write-Host "autoexec.cfg copied successfully."
